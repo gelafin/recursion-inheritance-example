32f5f5aacb6df3a25d2dd218d409e72353890725
Mark <themarkymarkers@gmail.com>
2020-10-28 11:26:24 -0700

    *poor man's cloud save*
    just saving progress; nothing new completed yet

--- recursion_inheritance.py
+++ recursion_inheritance.py
@@ -17,22 +17,18 @@ class HeightMap:
         :param ceiling: max height of sky and height of walls
         :param terrain_type: type of terrain to simulate. Default is gentle hills
         """
+        self._WALL_VARIANCE = 1  # how much higher walls are than the apex
+
         # if ceiling provided by caller, use that. Else, use the apex as the ceiling
         need_ceiling = False
         if ceiling is not None:
-            self._ceiling = self._WALL = ceiling  # convenient to have these different semantic terms
+            self._ceiling = self._WALL = ceiling + self._WALL_VARIANCE  # convenient to have different semantic terms
         else:
             need_ceiling = True
 
-        # set height of first column
-        starting_height = 0
-
         # generate list of height values
+        starting_height = 0  # height of first column
         self._height_values = self.generate_heightmap(length, terrain_type, starting_height)
-
-        # after generating height values, use the apex as the ceiling if not provided by caller
-        if need_ceiling:
-            self._ceiling = self._WALL = self.get_highest_point()
 
         # after generating height values, make another list guaranteed to have only non-negative values
         self._non_negative_heights = self._height_values
@@ -45,8 +41,16 @@ class HeightMap:
                 self._non_negative_heights.append(self._WALL)
                 break  # for efficiency
 
+        # after generating non-negative height values, use the apex to find the ceiling if not provided by caller
+        if need_ceiling:
+            self._ceiling = self._WALL = self.get_highest_point() + self._WALL_VARIANCE  # TODO: circular dependency between need_ceiling _WALL instantiation and _non_negative_heights instantiation
+
         # after generating height values, make a 2D version
         self._heightmap_2D = self.make_2D_copy_above_ground()  # depends on self._non_negative_heights
+
+    def get_wall_variance(self):
+        """:return wall variance: how much higher walls are than the highest point (apex)"""
+        return self._WALL_VARIANCE
 
     def get_height_values(self):
         """returns height values"""
@@ -56,12 +60,20 @@ class HeightMap:
         """returns list of height values, with 0 as a minimum value"""
         return self._non_negative_heights
 
-    def get_highest_point(self):
-        """returns highest point (apex) in heightmap"""
-        # default comparison to first height
-        maximum = self.get_height_values()[0]
+    def get_highest_point(self, heights=None):
+        """
+        returns highest point (apex) in heightmap
+        :param heights: list of height values
+        :return maximum: highest point (apex) in heights
+        """
+        # if heights is not provided, assume caller wants the highest point of the generated height values
+        if heights is None:
+            heights = self.get_height_values()
 
-        for height in self._height_values:
+        # default comparisons to first height
+        maximum = heights[0]
+
+        for height in heights:
             if height > maximum:
                 maximum = height
 
@@ -125,13 +137,13 @@ class HeightMap:
         :param heightmap: array of height values
         :param previous_height: used for recursion
         """
-        # handy helper constant for ranges
-        INCLUSIVE = 1
+        # handy helpers
+        INCLUSIVE = 1  # for ranges
 
         # base case: if length has been reached, we're done
         if len(heightmap) >= length:
             # add a wall to show the boundary
-            heightmap.append(self._WALL)
+            heightmap.append(self.get_highest_point(heightmap) + self.get_wall_variance())
             return heightmap
 
         # recursive case
@@ -158,6 +170,7 @@ class HeightMap:
     def generate_heightmap(self, length, terrain_type, starting_height):
         """
         generates a new, random heightmap as an array of height values for the HeightMap object
+        first column is 0 and last column is equal to the highest plus 1
         :param length: how many columns of terrain to generate
         :param terrain_type: type of terrain to generate, e.g., gentle_hills, mountains, etc.
         :param starting_height: desired height of leftmost column
@@ -179,5 +192,5 @@ class HeightMap:
         return self.recursive_generate_heightmap(length, max_variance, heightmap, previous_height=starting_height)
 
 # test
-heightmap = HeightMap(10, 10)
+heightmap = HeightMap(10)
 heightmap.print()
